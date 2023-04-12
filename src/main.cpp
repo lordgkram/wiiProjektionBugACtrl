826c8cd7a81469aed7a2d3ad9f4d99d3e510be68
lordgkram <52086058+lordgkram@users.noreply.github.com>
2023-04-12 19:02:35 +0200

    wii quality of live improvements

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,68 @@
 
 #include "fontUtil.hpp"
 
-#ifdef __wii__ // wii
+#ifdef __wii__
+#include "gccore.h"
+#include "wiiuse/wpad.h"
 #else /* __wii__ */
 #endif /* __wii__ */
 
 bool running = true;
 
-constexpr int32_t mspf = 1000 / 20;
-SDL_Color colors[256];
+constexpr int32_t mspf = 1000 / 60;
 
-#ifdef __wii__ // wii
+#ifdef __wii__
+// newimpl for wii (without cudnt link)
 void *operator new[](size_t v) { return malloc(v); }
 void operator delete[](void *v) { free(v); }
 void *operator new(size_t v) { return malloc(v); }
 void operator delete(void *v) { free(v); }
+
+// wii specific variables
+GXRModeObj *screenPrefMode;
+int8_t wiiExitState = -1;
 #endif /* __wii__ */
 
 #ifdef __cplusplus
 extern "C"
 #endif
 int main(int argc, char *argv[]) {
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) == -1) {
+#ifdef __wii__
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+#else /* __wii__ */
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) == -1)
+#endif /* __wii__ */
+    {
         return 1;
     }
+
+#ifdef __wii__
+    // init WPAD
+    WPAD_Init();
+    WPAD_SetPowerButtonCallback([](s32 chan) -> void {
+        running = false;
+        wiiExitState = SYS_POWEROFF_STANDBY;
+    });
+    SYS_SetResetCallback([](u32 irq, void* ctx) -> void {
+        running = false;
+        wiiExitState = SYS_RETURNTOMENU;
+    });
+    SYS_SetPowerCallback([]() -> void {
+        running = false;
+        wiiExitState = SYS_POWEROFF_STANDBY;
+    });
+    SDL_ShowCursor(SDL_DISABLE);
+#endif /* __wii__ */
 
     SDL_Surface *screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE);
     if(screen == nullptr) {
         SDL_Quit();
         return 2;
     }
-    for(uint16_t i = 0; i < 256; i++){
-        colors[i].r = i;
-        colors[i].g = i;
-        colors[i].b = i;
-    }
-    SDL_SetColors(screen, colors, 0, 256);
+    SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 80, 80, 80));
 
     loadFont();
-    uint16_t wx = 0, wy = 0;
-    wx = 0;
-    wy = 0;
+    uint16_t wx = 16, wy = 16;
     writeFont(screen, wx, wy, "SDL JOYSTICK OUT V1\n");
 
     char *writeBuff = new char[512];
@@ -64,11 +86,18 @@ int main(int argc, char *argv[]) {
         writeFont(screen, wx, wy, writeBuff);
         SDL_JoystickClose(joystick);
     }
-    delete[] writeBuff;
+    snprintf(writeBuff, 512, "%d BPX\n", screen->format->BytesPerPixel);
+    writeFont(screen, wx, wy, writeBuff);
+    snprintf(writeBuff, 512, "%s\n", (screen->flags & SDL_HWSURFACE) ? "is HW" : "is not HW");
+    writeFont(screen, wx, wy, writeBuff);
     
     SDL_Event event;
     uint32_t startMs = SDL_GetTicks();
+    int e = 0;
     while(running) {
+        //SDL_PumpEvents();
+#ifndef __wii__
+        // SDL input
         while(SDL_PollEvent(&event)) {
             switch(event.type) {
             case SDL_QUIT:
@@ -76,18 +105,37 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
+#else /* ! __wii__ */
+        // wii input
+        // get WiiFB events
+        WPAD_ScanPads();
+        // exit when homebutton on WiiFB1 is pressed
+        if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) running = false;
+#endif /* ! __wii__ */
+        wx = 16;
+        snprintf(writeBuff, 512, "%d", e);
+        writeFont(screen, wx, wy, writeBuff);
 
         SDL_Flip(screen);
+        e++;
 
+#ifndef __wii__
         // frame limit
         uint32_t endMs = SDL_GetTicks();
         int32_t delay = mspf - (endMs - startMs);
         if(delay > 0) SDL_Delay(delay);
         startMs = endMs;
+#endif /* ! __wii__ */
     }
 
     // shutdown
+    delete[] writeBuff;
     deleteFont();
     SDL_Quit();
+#ifdef __wii__
+    if(wiiExitState != -1) SYS_ResetSystem(wiiExitState, 0, 0); // do what the button toldus
+    else if(argc > 0) exit(0); //return to homebrew channel
+    else SYS_ResetSystem(SYS_RETURNTOMENU, 0, 0); // return to main menu
+#endif /* __wii__ */
     return 0;
 }
